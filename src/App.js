ffa4cbc46d05c3d899bfa5bfebf76e3724f4a2e5
Precious <Preshomondi@gmail.com>
2022-11-14 12:44:43 +0300

    import useSelector and display the states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import React from "react"
+import {useSelector} from "react-redux"
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
   return (
     <div className="App">
-   <h1>Hello</h1>
+   <h1>Counter {counter}</h1>
+   <button>+</button>
+   <button>-</button>
+   {isLogged ? <h3>Valuable info not to be seen</h3> : "" }
+   
     </div>
   );
 }
